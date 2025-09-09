5c1f33146f75cd30239a2cca00f99a61ad19d29a
kathrinmzl <kathrin@muenzel.de>
2025-09-09 15:30:53 +0200

    Migrate Comment model and add Comment model to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # "." indicates that we are importing Post from a file named models, 
 # which is in the same directory as our admin.py file
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 # Show Post model on admin site
 admin.site.register(Post)
+admin.site.register(Comment)
