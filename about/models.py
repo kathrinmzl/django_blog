d00e0c2b62473d3493257cd878bd98cfbdc31674
kathrinmzl <kathrin@muenzel.de>
2025-09-15 15:47:11 +0200

    Add custom image to About page

--- about/models.py
+++ about/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    profile_image = CloudinaryField('image', default = "placeholder")
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
