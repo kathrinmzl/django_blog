54bde3dc21a948e522b56e034b2dde6ccf492df4
kathrinmzl <kathrin@muenzel.de>
2025-09-15 11:03:06 +0200

    Add collaboration form to About page

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 
 def about_me(request):
@@ -8,8 +10,24 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
 
+    # form functionality
+    if request.method == "POST":
+        # print("Received a POST request")
+        collab_form = CollaborateForm(data=request.POST)
+        if collab_form.is_valid():
+            collab_form.save()
+           
+            # Success message
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
+    collab_form = CollaborateForm()
+
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         "collab_form": collab_form},
     )
